614e2fee23d3acc0221fa524c101a1aff55ffdba
zigron <zeeshan.arshad@zigron.com>
2020-12-09 12:48:41 +0500

    user defined array added

--- src/components/LargestNo.tsx
+++ src/components/LargestNo.tsx
@@ -1,49 +1,88 @@
 import React, { useEffect, useState } from 'react';
 import { Form ,Button,Container,Row,Col} from "react-bootstrap";
 import {apiRequestAxio} from '../services/api/apiClass'
+let noArr:any = [];
+let noHolder:any = '';
 const LargestNo = () =>{
-    const [largestNo ,setLargestNo] = useState('')
-    const [type ,setType] = useState('')
+    const [largestNo ,setLargestNo] = useState('');
+    const [type ,setType] = useState('');
+    const [noArrayElement , setNoArrayElement] = useState('');
     useEffect(() => {
         
     },[])
-    const clickMe = async () => {
-        console.log(type)
+    const submitType = async () => {
         const body = {
-            "input_array" : [14,2,34,444,2,39]
+            "input_array" : noArr
         } 
         try {
             let response = await apiRequestAxio("POST", `api-get-no/`,body, { "Content-Type": "application/json" });
             if(response.data){
-                type === 'largest'?setLargestNo(response.data.largest_no):setLargestNo(response.data.second_largest)   
+                if(type === 'largest')
+                {
+                    setLargestNo(response.data.largest_no)
+                }
+                else if (type === 'secondlargest')
+                {
+                    setLargestNo(response.data.second_largest)   
+                }
             }
         }catch (e) {
             console.error(e);
             return null;
         }
     }
-    
+    const collectArray = () =>{
+        noArr.push(noArrayElement)
+        noHolder = ''
+        noArr.map((value:any)=>{
+            noHolder += `${value},`;
+        })
+        setNoArrayElement('');
+    }
     return (
         <React.Fragment>
             <Container fluid>
             <Row>
                 <Col xs={{ span: 6, offset: 3 }}>
-                    <label>Your array is <p className={`array-color`}>[14,2,34,444,2,39]</p></label>
+                    <h1>Bentley Task</h1>
                 </Col>
+            </Row>
+            <Row>
+
+                <Col xs={{ span: 6, offset: 3 }}>
+                    <Form.Group controlId="formArrayElement">
+                    <Form.Label>Please Enter the values in array</Form.Label>
+                    <Form.Control type='text' value={noArrayElement} onChange={(e)=>setNoArrayElement(e.target.value)} placeholder='Add element to the array' />
+                    </Form.Group>
+                    <div>
+                        <Button onClick={collectArray} variant="outline-warning">Add No</Button>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                   
             </Row>
             <Row>
                 <Col xs={{ span: 6, offset: 3 }}>
                     <Form.Group controlId="exampleForm.ControlSelect1">
                         <Form.Label>Please Select Your Choice</Form.Label>
-                        <Form.Control as="select" onChange = {(e) => setType(e.target.value)}>
+                        <Form.Control as="select" onChange = {(e) => setType(e.target.value)} >
                             <option disabled selected>Select...</option>
                             <option value="largest">Largest</option>
-                            <option value="slargest">Second Largest</option>
+                            <option value="secondlargest">Second Largest</option>
                         </Form.Control>
                     </Form.Group>
                     <div>
-                        <Button onClick ={clickMe} variant="outline-warning" disabled={type!==''?false:true}>Find Number</Button>
+                        <Button onClick ={submitType} variant="outline-warning" disabled={type!==''?false:true}>Find Number</Button>
                     </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={{ span: 6, offset: 3 }}>
+                    {noHolder !== ''
+                        ?<label>{`Your Array is ${noHolder}`}</label>
+                        :null
+                    }
                 </Col>
             </Row>
             <Row>
